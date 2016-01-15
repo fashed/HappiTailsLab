3572114474d46c8cdcfaebb80e7abb70adde5244
Jennifer <fashed@gmail.com>
2016-01-14 19:01:28 -0500

    modified animal.rb

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,12 +1,14 @@
 class Animal < ActiveRecord::Base
   belongs_to :shelter
-  has_one :client
+  belongs_to :client
 
 	def owner
-		if self.shelter
-			shelter.name
-		else
+		if self.client
 			self.client.name
+		elsif self.shelter
+			self.shelter.name
+		else
+			"I'm orphaned"
 		end
 	end
 
